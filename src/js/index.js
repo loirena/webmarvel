39759d3856fdecfb0a59e2300000f5afb892f642
unknown <dlm.lorena@gmail.com>
2022-07-09 13:32:09 -0300

    marveltela

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        const
+
+
+    })
+})
+
